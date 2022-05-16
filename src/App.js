4d88fd762e342eb9dadb30aac4205d409689dd9e
Robert Hogan <rhogan7@gmail.com>
2022-05-16 12:58:15 -0700

    Added media queries for project.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
 import Header from './components/header/Header';
 import Loading from './components/loading/Loading';
+import Card from './/components/card/Card';
+
+import StarWarsLogo from './Starwars-logo.webp';
 import './App.css';
 import './components/pagination/pagination.css'
 
@@ -9,7 +12,7 @@ function App() {
   const [ loading, setLoading ] = useState(true);
   const [ data, setData ] = useState([]);
   const [ character, setCharacter ] = useState('');
-
+  const [ searchResults, setSearchResults ] = useState(false);
 
 useEffect(() => {
   getData();
@@ -28,6 +31,7 @@ useEffect(() => {
     const json = await response.json();
     setData(json.results);
     setLoading(false);
+    setSearchResults(true);
   }
 
   return (
@@ -36,10 +40,10 @@ useEffect(() => {
       <img
           width="320"
           className="mx-auto"
-          src="http://vignette1.wikia.nocookie.net/disney/images/8/8b/Starwars-logo.png"
+          src={StarWarsLogo}
           alt="Star Wars Logo"
         />
-      <div className="pagination">
+      <div className="pagination container">
            <form className="form" onSubmit={(e) => {
             e.preventDefault();
             searchData();
@@ -50,20 +54,44 @@ useEffect(() => {
             </label>
             <button type="submit">Search</button>
             </form>
-        </div>
-      <div className="card-grid">
+            {searchResults ? <p className="block text-center search-results">{data.length} results found</p> : ''}
+            </div>
+      <div className="container cardGrid">
       {loading ? (
         <Loading />
       ) : (
-        <div>
-        {data.map(character => (
-          <div className="card" key={character.name}>
+        <>
+          {data.map((character) => (
+            <div
+              className="card"
+              title={character.name}
+              key={character.name}
+            >
+            <div className="card-inner">
+              <h3 className="card-title">{character.name}</h3>
+              <ul>
+                <li>Mass: {character.mass}</li>
+                <li>Height: {character.height}</li>
+                <li>Hair Color: {character.hair_color}</li>
+                <li>Skin Color: {character.skin_color}</li>
+                <li>Eye Color: {character.eye_color}</li>
+                <li>Birth Year: {character.birth_year}</li>
+                <li>Gender: {character.gender}</li>
+              </ul>
+              </div>
+            </div>
+          ))}
+
+
+
+
+          {/* <div className="card" key={character.name}>
+          
               <ul>
                 <li>{character.name}</li>
               </ul>
-          </div>
-        ))}
-        </div>
+          </div> */}
+        </>
       )}
 
       </div>
